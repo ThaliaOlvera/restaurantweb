8506ba1feeb1b03ff3e43e8b9039a558fda3758b
Thalia Olvera <thalia_olvera@hotmail.com>
2022-08-23 10:31:09 -0500

    Se añade Find Us

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import {Header} from './components/Headerbar/Header';
 import {AboutUs} from './container/AboutUs/AboutUs';
 import Chef from './container/Chef/Chef'
 import Gallery from './container/Gallery/Gallery'
+import FindUs from './container/Findus/FindUs'
+import Footer from './container/Footer/Footer'
 import './App.css';
 
 const App = () => (
@@ -14,9 +16,8 @@ const App = () => (
     <AboutUs />
     <Chef />
     <Gallery />
-
-     {/*<FindUs />
-    <Footer />  */}
+    <FindUs />
+    <Footer /> 
   </div>
 );
 
--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import {SubHeading}  from '../../components';
+import {images} from '../../constants';
+
 
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className='app__bg app__wrapper section__padding' id='contact'>
+    <div className='app__wrapper_info'>
+      <SubHeading title="Contacto"/>
+      <h1 className="headtext__cormorant" style={{marginBottom: '3rem'}}>Ubicanos</h1>
+     <div className="app__wrapper-content">
+     <p className="p__opensans">Calle Mero #12, Av Carlos Nader, 77500 </p>
+     <p className="p__opensans">Correo: Gabs@gabsrestaurant.com </p>
+     <p className="p__cormorant" style={{color: '#DCCA87', margin: '2rem 0' }}>Horario de Apertura</p>
+     <p className="p__opensans">Martes a Domingo: 10:00am- 11:30pm</p>
+     <p className="p__opensans">Dias Festivos: 8am-8pm.</p>
+     </div> 
+     <button className='custom__button' style={{marginTop: '2rem'}}>Visitanos</button>   
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.findus} alt="findUs imagen" />
+    </div>
   </div>
 );
 
